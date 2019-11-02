51b3c3766bfce8be7ccc311188039e5bdef7048a
AJ Foster <web@aj-foster.com>
2019-11-01 22:55:18 -0400

    Add runtime tools to enable remote observer

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule FTC.Overseer.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:logger, :runtime_tools],
       mod: {FTC.Overseer.Application, []}
     ]
   end
