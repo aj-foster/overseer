be1bb27363976d4d0e8a1e45d1b5bd72946d55d7
AJ Foster <web@aj-foster.com>
2019-12-16 20:18:09 -0500

    Add missing tuple to mock runner response

--- lib/ftc/overseer/executor/mock_runner.ex
+++ lib/ftc/overseer/executor/mock_runner.ex
@@ -25,17 +25,18 @@ defmodule FTC.Overseer.Executor.MockRunner do
   end
 
   def run("./bin/scan" <> _, _opts) do
-    """
-    00:00:00:00:00:01 | 1 | -40 | "DIRECT-AA-1-RC"
-    00:00:00:00:00:02 | 6 | -40 | "DIRECT-BB-2-RC"
-    00:00:00:00:00:03 | 11 | -40 | "DIRECT-CC-3-RC"
-    00:00:00:00:00:04 | 157 | -40 | "DIRECT-DD-4-RC"
-    00:00:00:00:00:05 | 1 | -40 | "DIRECT-EE-5-RC"
-    00:00:00:00:00:06 | 6 | -40 | "DIRECT-FF-6-RC"
-    00:00:00:00:00:07 | 11 | -40 | "DIRECT-GG-7-RC"
-    00:00:00:00:00:08 | 157 | -40 | "DIRECT-HH-8-RC"
-    00:00:00:00:00:09 | 1 | -80 | "DIRECT-AA-1-B-RC"
-    """
+    {:ok,
+     """
+     00:00:00:00:00:01 | 1 | -40 | "DIRECT-AA-1-RC"
+     00:00:00:00:00:02 | 6 | -40 | "DIRECT-BB-2-RC"
+     00:00:00:00:00:03 | 11 | -40 | "DIRECT-CC-3-RC"
+     00:00:00:00:00:04 | 157 | -40 | "DIRECT-DD-4-RC"
+     00:00:00:00:00:05 | 1 | -40 | "DIRECT-EE-5-RC"
+     00:00:00:00:00:06 | 6 | -40 | "DIRECT-FF-6-RC"
+     00:00:00:00:00:07 | 11 | -40 | "DIRECT-GG-7-RC"
+     00:00:00:00:00:08 | 157 | -40 | "DIRECT-HH-8-RC"
+     00:00:00:00:00:09 | 1 | -80 | "DIRECT-AA-1-B-RC"
+     """}
   end
 
   def run(_command, _opts), do: {:error, "Not implemented", 1}
