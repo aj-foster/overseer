41fe0337dc5343f73187b05639cbed0bebecf519
AJ Foster <web@aj-foster.com>
2020-01-06 21:13:12 -0500

    Add nerves-specific configuration

--- config/prod.exs
+++ config/prod.exs
@@ -2,6 +2,6 @@ use Mix.Config
 
 config :overseer, FTC.Display.Endpoint,
   server: true,
-  http: [:inet6, port: String.to_integer(System.get_env("PORT") || "4000")],
+  http: [:inet6, port: 4000],
   url: [host: "example.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
