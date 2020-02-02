2bbf6d7659b96264f790850173cd81112d46e2f6
AJ Foster <web@aj-foster.com>
2020-02-02 00:49:20 -0500

    Stop match timer during match aborts
    Resolves #3.

--- lib/ftc/overseer/match_manager.ex
+++ lib/ftc/overseer/match_manager.ex
@@ -50,29 +50,31 @@ defmodule FTC.Overseer.MatchManager do
   def handle_cast({:start, match_name}, _state) do
     Logger.info("Match start: #{match_name}")
     Status.start_match(match_name)
-    Process.send_after(self(), :stop, @match_length_ms)
+    timer = Process.send_after(self(), :stop, @match_length_ms)
 
     case Scorekeeper.get_active_match() do
       {:ok, %Match{name: ^match_name, teams: teams} = match} ->
         Status.set_teams(teams)
         WLAN.observe(teams)
-        {:noreply, match}
+        {:noreply, %{match | timer: timer}}
 
       {:ok, %Match{name: name, teams: teams} = match} ->
         Logger.error("Expected match #{match_name} but current active match is #{name}")
 
         Status.set_teams(teams)
         WLAN.observe(teams)
-        {:noreply, match}
+        {:noreply, %{match | timer: timer}}
 
       other ->
         Logger.error("Could not get data for active match. Got #{inspect(other)}")
+        Process.cancel_timer(timer)
         {:noreply, :inactive}
     end
   end
 
-  def handle_cast(:abort, %Match{name: name}) do
+  def handle_cast(:abort, %Match{name: name, timer: timer}) do
     Logger.info("Match abort: #{name}")
+    Process.cancel_timer(timer)
     Status.abort_match()
 
     WLAN.stop_all()
