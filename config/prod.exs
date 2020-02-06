2c70b75b953c27c9ccd5110cc0ba26b4d9a30bda
AJ Foster <web@aj-foster.com>
2020-02-05 22:58:28 -0500

    Add file backend for logging

--- config/prod.exs
+++ config/prod.exs
@@ -9,3 +9,9 @@ config :overseer, FTC.Display.Endpoint,
   http: [:inet6, port: 4000],
   url: [host: "example.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
+
+config :logger, backends: [:console, {LoggerFileBackend, :debug_log}]
+
+config :logger, :debug_log,
+  path: "/var/log/overseer.log",
+  level: :debug
