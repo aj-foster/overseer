ebf54c37772eb525c4e4e0d03f75364f808e73b9
AJ Foster <web@aj-foster.com>
2020-03-22 17:46:30 -0400

    Pre-populate status page and display channel/packet counts

--- lib/ftc/display/live/status_live.ex
+++ lib/ftc/display/live/status_live.ex
@@ -22,7 +22,7 @@ defmodule FTC.Display.StatusLive do
           |> assign(:status, "In Progress")
           |> assign(:match, match.name)
           |> assign(:teams, transpose_teams(match.teams))
-          |> assign(:statuses, %{})
+          |> populate_teams(match.name, match.teams)
 
         {:ok, socket}
 
@@ -69,29 +69,31 @@ defmodule FTC.Display.StatusLive do
   end
 
   # Received information about which teams are playing.
-  def handle_info({:populated, _match_name, teams}, socket) do
-    s = "searching"
-    [red1, blue1, red2, blue2] = transpose_teams(teams)
-
-    socket =
-      socket
-      |> assign(:teams, [red1, blue1, red2, blue2])
-      |> assign(:statuses, %{red1 => s, red2 => s, blue1 => s, blue2 => s})
-
-    {:noreply, socket}
+  def handle_info({:populated, match, teams}, socket) do
+    {:noreply, populate_teams(socket, match, teams)}
   end
 
   # Tracking of a team has begun.
-  def handle_info({:found, team, _channel}, socket) do
-    statuses = Map.put(socket.assigns[:statuses], team, "tracking")
+  def handle_info({:found, team_number, channel}, socket) do
+    team_info =
+      Map.get(socket.assigns[:statuses], team_number, %{})
+      |> Map.put(:channel, channel)
+      |> Map.put_new(:status, "tracking")
+
+    statuses = Map.put(socket.assigns[:statuses], team_number, team_info)
     socket = assign(socket, :statuses, statuses)
 
     {:noreply, socket}
   end
 
   # An issue with a team has been detected.
-  def handle_info({:deauthenticated, team, _total_count}, socket) do
-    statuses = Map.put(socket.assigns[:statuses], team, "problem")
+  def handle_info({:deauthenticated, team_number, total_count}, socket) do
+    team_info =
+      Map.get(socket.assigns[:statuses], team_number, %{})
+      |> Map.put(:packets, total_count)
+      |> Map.put(:status, "problem")
+
+    statuses = Map.put(socket.assigns[:statuses], team_number, team_info)
     socket = assign(socket, :statuses, statuses)
 
     {:noreply, socket}
@@ -99,7 +101,38 @@ defmodule FTC.Display.StatusLive do
 
   def handle_info(_message, socket), do: {:noreply, socket}
 
+  # Assign team numbers and statuses.
+  #
+  @spec populate_teams(Socket.t(), String.t(), [pos_integer]) :: Socket.t()
+  defp populate_teams(socket, match, teams) do
+    Enum.reduce(teams, socket, fn team_number, socket ->
+      team_info =
+        case FTC.Overseer.get_team(match, team_number) do
+          {:ok, %{packets: x} = team_info} when is_integer(x) and x > 0 ->
+            Map.put(team_info, :status, "problem")
+
+          {:ok, %{channel: x} = team_info} when is_integer(x) ->
+            Map.put(team_info, :status, "tracking")
+
+          {:ok, team_info} ->
+            team_info
+
+          {:error, :not_found} ->
+            %{}
+        end
+
+      statuses =
+        Map.get(socket.assigns, :statuses, %{})
+        |> Map.put(team_number, team_info)
+
+      assign(socket, :statuses, statuses)
+    end)
+    |> assign(:teams, transpose_teams(teams))
+  end
+
   # Reposition teams to appear similar to their physical orientation.
+  #
+  @spec transpose_teams([pos_integer]) :: [pos_integer]
   defp transpose_teams([blue1, blue2, red1, red2]), do: [red1, blue1, red2, blue2]
 
   defp transpose_teams(teams) do
--- lib/ftc/overseer.ex
+++ lib/ftc/overseer.ex
@@ -6,6 +6,7 @@ defmodule FTC.Overseer do
   defdelegate set_api_host(host), to: FTC.Overseer.Scorekeeper
   defdelegate set_event_code(code), to: FTC.Overseer.Scorekeeper
   defdelegate get_match(), to: FTC.Overseer.Match
+  defdelegate get_team(match, team), to: FTC.Overseer.Event.Ledger
 
   @doc """
   Stop the application.
